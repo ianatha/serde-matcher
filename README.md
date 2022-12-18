f5cb9c253d6a0bf463c2f989a721e6a6b78e53b7
Ian Atha <ian@atha.io>
2022-12-18 20:59:05 +0200

    docs

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# serde-matcher
+# serde_json_matcher
+
+Implements a matcher for `serde_json::Value`s using the MongoDB query language in Rust.
+
+Currently supports `$eq`, `$in`, `$ne`, `$nin`, `$and`, `$not`, `$or`, `$type` and `$nor`.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! # serde_json_matcher
+//!
+//! Implements a matcher for `serde_json::Value`s using the MongoDB query language.
+//!
+//! Currently supports `$eq`, `$in`, `$ne`, `$nin`, `$and`, `$not`, `$or`, `$type` and `$nor`.
+
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
